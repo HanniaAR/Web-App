22c6ed69f77cef3ace8f22f0c8f78eed9425666b
HanniaAR <hanniarias.r10@gmail.com>
2024-01-24 16:28:24 -0600

    Create users method and class

---
+++ console_app/usuarios/usuario.py
@@ -0,0 +1,13 @@
+class Usuario:
+
+    def __init__(self, name, last_name, email, password) -> None:
+        self.name = name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+    def register(self):
+        pass
+
+    def identify(self):
+        pass
