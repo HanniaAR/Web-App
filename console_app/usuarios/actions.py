7933ac8dd66cf8760e1d5ac772c223ac9ed2f520
HanniaAR <hanniarias.r10@gmail.com>
2024-01-24 21:56:09 -0600

    Add instance from usuario to register new user

--- console_app/usuarios/actions.py
+++ console_app/usuarios/actions.py
@@ -1,3 +1,4 @@
+from usuarios.usuario import Usuario
 class Actions:
     
     def register(self):
@@ -6,6 +7,14 @@ class Actions:
         last_name = input('Write your last name: ')
         email = input('Write your email: ')
         password = input('Write your password: ')
+
+        usuario = Usuario(name, last_name, email, password)
+        add = usuario.register()
+
+        if add[0] >= 1:
+            print(f'Se ha registrado {add[1].name} con el email {add[1].email}')
+        else:
+            print('No se ha registrado correctamente')
     
     def login(self):
         print('\nWrite your credentianls')
